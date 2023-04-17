086d5e8508bca6551ad626244606868209188fae
Dwarf1er <SerolTech@protonmail.com>
2023-04-16 21:18:01 -0400

    Added skill cards

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import calculateRowHeight from "../../utils/calculateRowHeight";
 
 function Skills() {
@@ -21,7 +19,80 @@ function Skills() {
       <Row className="align-items-center" style={{ height: rowHeight }}>
         <Col md={4} className="text-start mx-auto">
           <h1 className="display-5 fw-bold">Skills</h1>
-          <h2>SKILLS</h2>
+        </Col>
+      </Row>
+      <Row
+        className="align-items-center justify-content-center"
+        style={{ height: rowHeight }}
+      >
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/html5-original-wordmark.svg"
+            />
+          </Card>
+        </Col>
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/css3-original-wordmark.svg"
+            />
+          </Card>
+        </Col>
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/typescript-original.svg"
+            />
+          </Card>
+        </Col>
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/react-original-wordmark.svg"
+            />
+          </Card>
+        </Col>
+      </Row>
+      <Row
+        className="align-items-center justify-content-center"
+        style={{ height: rowHeight }}
+      >
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/nodejs-original-wordmark.svg"
+            />
+          </Card>
+        </Col>
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/csharp-original.svg"
+            />
+          </Card>
+        </Col>
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/dotnetcore.png"
+            />
+          </Card>
+        </Col>
+        <Col md={2}>
+          <Card className="border-0" style={{ width: "10rem" }}>
+            <Card.Img
+              variant="top"
+              src="https://profilinator.rishav.dev/skills-assets/git-scm-icon.svg"
+            />
+          </Card>
         </Col>
       </Row>
     </Container>
